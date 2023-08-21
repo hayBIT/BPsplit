870097ab67db9417659ab89efb1dce615cad49fd
mokkin <markus@haybach.com>
2023-08-22 00:10:04 +0200

    Create config.sample.php

---
+++ config.sample.php
@@ -0,0 +1,20 @@
+<?php
+
+$config = [
+    'smtp_host' => 'smtp.example.com',
+    'smtp_username' => 'your_username',
+    'smtp_password' => 'your_password',
+    'smtp_secure' => 'tls',
+    'smtp_port' => 587,
+    'from_email' => 'your_email@example.com',
+    'from_name' => 'Your Name',
+    'max_attachments' => 5,
+];
+
+$recipient_emails = [
+    '700' => 'test700@example.com',
+    '900' => 'test900@example.com',
+    '1100' => 'test1100@example.com'
+];
+
+?>
